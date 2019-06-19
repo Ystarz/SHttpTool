c43e0a5629ac6182a0566358be1d593c11f714e6
sss <sujianfu199@qq.com>
2019-06-19 17:24:10 +0800

    Initial Commit

---
+++ SHttpTool/SHttpTool.h
@@ -0,0 +1,19 @@
+//
+//  SHttpTool.h
+//  SHttpTool
+//
+//  Created by SSS on 2019/6/19.
+//  Copyright © 2019 SSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SHttpTool.
+FOUNDATION_EXPORT double SHttpToolVersionNumber;
+
+//! Project version string for SHttpTool.
+FOUNDATION_EXPORT const unsigned char SHttpToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHttpTool/PublicHeader.h>
+
+
